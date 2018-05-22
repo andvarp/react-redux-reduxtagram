a5857bc0c69679bcf940adae13f7f22c6728f7d5
Andres Vargas <jose.vargas@prodigious.cr>
2018-05-21 22:39:35 -0600

    Adding redux support

---
+++ client/reducers/posts.js
@@ -0,0 +1,11 @@
+// 2 things
+
+// 1 - the action -- what happend!?
+// 2 - copy of current state
+
+function posts(state = [], action) {
+  console.log(state, action)
+  return state
+}
+
+export default posts;
